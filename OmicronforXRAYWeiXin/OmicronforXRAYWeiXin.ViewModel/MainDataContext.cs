f27c1a85531b97239999cf5a3da0a97bf1ea0811
sunxinjian <xinjian_sun@sina.com>
2017-09-02 14:50:59 +0800

    初始工作1

---
+++ OmicronforXRAYWeiXin/OmicronforXRAYWeiXin.ViewModel/MainDataContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BingLibrary.hjb;
+using BingLibrary.hjb.Intercepts;
+using System.ComponentModel.Composition;
+
+namespace OmicronforXRAYWeiXin.ViewModel
+{
+    [BingAutoNotify]
+    public class MainDataContext : DataSource
+    {
+        #region 属性
+        public virtual string MsgText { set; get; } = "";
+        public virtual string BarcodeString1 { set; get; }
+        public virtual string BarcodeString2 { set; get; }
+        public virtual string HomePageVisibility { set; get; } = "Visible";
+        public virtual string ParameterPageVisibility { set; get; } = "Collapsed";
+        #endregion
+        #region 变量
+        private string MessageStr = "";
+        #endregion
+        #region 功能与方法
+        public void ScanAction()
+        {
+            MsgText = AddMessage("Scan");
+        }
+        private string AddMessage(string str)
+        {
+            string[] s = MessageStr.Split('\n');
+            if (s.Length > 1000)
+            {
+                MessageStr = "";
+            }
+            MessageStr += "\n" + System.DateTime.Now.ToString() + " " + str;
+            return MessageStr;
+        }
+        #endregion
+    }
+    class VMManager
+    {
+        [Export(MEF.Contracts.Data)]
+        [ExportMetadata(MEF.Key, "md")]
+        MainDataContext md = MainDataContext.New<MainDataContext>();
+    }
+}
--- OmicronforXRAYWeiXin/OmicronforXRAYWeiXin/MainWindow.xaml.cs
+++ OmicronforXRAYWeiXin/OmicronforXRAYWeiXin/MainWindow.xaml.cs
@@ -12,17 +12,60 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using OmicronforXRAYWeiXin.ViewModel;
 
 namespace OmicronforXRAYWeiXin
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
+        private dialog mydialog = new dialog();
         public MainWindow()
         {
             InitializeComponent();
+            #region 判断系统是否已启动
+
+            System.Diagnostics.Process[] myProcesses = System.Diagnostics.Process.GetProcessesByName("OmicronforXRAYWeiXin");//获取指定的进程名   
+            if (myProcesses.Length > 1) //如果可以获取到知道的进程名则说明已经启动
+            {
+                System.Windows.MessageBox.Show("不允许重复打开软件");
+                System.Windows.Application.Current.Shutdown();
+            }
+
+
+            #endregion
+        }
+
+        private void MetroWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            WindowClose();
+        }
+        private async void WindowClose()
+        {
+            try
+            {
+                mydialog.changeaccent("Red");
+
+                var r = await mydialog.showconfirm("确定要关闭程序吗？");
+                if (r)
+                {
+                    System.Windows.Application.Current.Shutdown();
+                }
+                else
+                {
+                    mydialog.changeaccent("Cobalt");
+                }
+            }
+            catch
+            {
+
+
+            }
+
         }
     }
 }
