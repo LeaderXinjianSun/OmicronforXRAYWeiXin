c373536300366bc844b6e160a53bc48d7fa68cfa
sunxinjian <xinjian_sun@sina.com>
2017-09-04 10:38:27 +0800

    基础功能完成

--- OmicronforXRAYWeiXin/OmicronforXRAYWeiXin.ViewModel/MainDataContext.cs
+++ OmicronforXRAYWeiXin/OmicronforXRAYWeiXin.ViewModel/MainDataContext.cs
@@ -18,14 +18,50 @@ namespace OmicronforXRAYWeiXin.ViewModel
         public virtual string BarcodeString2 { set; get; }
         public virtual string HomePageVisibility { set; get; } = "Visible";
         public virtual string ParameterPageVisibility { set; get; } = "Collapsed";
+        public virtual string ScanCom { set; get; }
+        public virtual string SerialPortCom { set; get; }
+        public virtual bool IsPLCConnect { set; get; } = false;
+        public virtual bool IsScanConnect { set; get; } = false;
         #endregion
         #region 变量
         private string MessageStr = "";
+        private XinjiePlc XinjiePLC;
+        private string iniParameterPath = System.Environment.CurrentDirectory + "\\Parameter.ini";
+        #endregion
+        #region 构造函数
+        public MainDataContext()
+        {
+            ScanCom = Inifile.INIGetStringValue(iniParameterPath, "Com", "ScanCom", "COM1");
+            SerialPortCom = Inifile.INIGetStringValue(iniParameterPath, "Com", "SerialPortCom", "COM1");
+            Scan.ini("ScanCom");
+        }
         #endregion
         #region 功能与方法
         public void ScanAction()
         {
-            MsgText = AddMessage("Scan");
+            Scan.GetBarCode(ScanActionCallback);
+        }
+        private void ScanActionCallback(string str)
+        {
+            if (str != "Error")
+            {
+                BarcodeString1 = str;
+                MsgText = AddMessage("扫码成功：" + str);
+            }
+            else
+            {
+                MsgText = AddMessage("扫码失败：" + str);
+            }           
+        }
+        public void ChooseHomePage()
+        {
+            HomePageVisibility = "Visible";
+            ParameterPageVisibility = "Collapsed";
+        }
+        public void ChooseParameterPage()
+        {
+            HomePageVisibility = "Collapsed";
+            ParameterPageVisibility = "Visible";
         }
         private string AddMessage(string str)
         {
@@ -36,6 +72,49 @@ namespace OmicronforXRAYWeiXin.ViewModel
             }
             MessageStr += "\n" + System.DateTime.Now.ToString() + " " + str;
             return MessageStr;
+        }
+        #endregion
+        #region PLC
+        [Initialize]
+        public void PLCWork()
+        {
+            while (true)
+            {
+                System.Threading.Thread.Sleep(100);
+                if (!IsPLCConnect)
+                {
+                    if (XinjiePLC != null)
+                    {
+                        XinjiePLC.DisConnectPLC();
+                    }
+                    try
+                    {
+                        XinjiePLC = new XinjiePlc(SerialPortCom, 19200, System.IO.Ports.Parity.Even, 8, System.IO.Ports.StopBits.One);
+                        IsPLCConnect = XinjiePLC.ConnectPLC();
+                    }
+                    catch
+                    {
+
+                    }
+                    if (IsPLCConnect)
+                    {
+                        IsPLCConnect = XinjiePLC.ReadM(24576);
+                    }
+                }
+                else
+                {
+                    IsPLCConnect = XinjiePLC.ReadM(24576);
+                }
+            }
+        }
+        [Initialize]
+        public void UpdateUI()
+        {
+            while (true)
+            {
+                System.Threading.Thread.Sleep(200);
+                IsScanConnect = Scan.State;
+            }
         }
         #endregion
     }
---
+++ OmicronforXRAYWeiXin/OmicronforXRAYWeiXin/ParameterPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO.Ports;
+using BingLibrary.hjb;
+
+namespace OmicronforXRAYWeiXin
+{
+    /// <summary>
+    /// ParameterPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class ParameterPage : UserControl
+    {
+        private string iniParameterPath = System.Environment.CurrentDirectory + "\\Parameter.ini";
+        public ParameterPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_DropDownOpened(object sender, EventArgs e)
+        {
+            var validComNames = SerialPort.GetPortNames();
+            foreach (var comName in validComNames)
+            {
+                if (!Com.Items.Contains(comName))
+                    Com.Items.Add(comName);
+            }
+            List<string> toRemove = new List<string>();
+            foreach (string addedName in Com.Items)
+            {
+                if (!validComNames.Contains(addedName))
+                    toRemove.Add(addedName);
+            }
+            foreach (string remove in toRemove)
+            {
+                Com.Items.Remove(remove);
+            }
+        }
+
+        private void Com1_DropDownOpened(object sender, EventArgs e)
+        {
+            var validComNames = SerialPort.GetPortNames();
+            foreach (var comName in validComNames)
+            {
+                if (!Com1.Items.Contains(comName))
+                    Com1.Items.Add(comName);
+            }
+            List<string> toRemove = new List<string>();
+            foreach (string addedName in Com1.Items)
+            {
+                if (!validComNames.Contains(addedName))
+                    toRemove.Add(addedName);
+            }
+            foreach (string remove in toRemove)
+            {
+                Com1.Items.Remove(remove);
+            }
+        }
+
+        private void Com1_DropDownClosed(object sender, EventArgs e)
+        {
+            Inifile.INIWriteValue(iniParameterPath, "Com", "ScanCom", Com1.Text);
+        }
+
+        private void Com_DropDownClosed(object sender, EventArgs e)
+        {
+            Inifile.INIWriteValue(iniParameterPath, "Com", "SerialPortCom", Com.Text);
+        }
+    }
+}
